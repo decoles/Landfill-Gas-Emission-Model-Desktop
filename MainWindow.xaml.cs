0fd54828c937f4ace3b5c72574cb67f8700fafab
David Coles <davidepcoles@gmail.com>
2023-03-15 01:02:55 -0700

    Started Implementing adding year

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
+using System.IO;
 
 namespace lwrncLandgemWPF
 {
@@ -23,26 +25,80 @@ namespace lwrncLandgemWPF
         public MainWindow()
         {
             InitializeComponent();
-        }
+            var file = @"content\pollutants.csv";
+            var lines = File.ReadAllLines(file);
+            foreach ( var line in lines)
+            {
+                var tokens = line.Split(',');
+                comboPollutant.Items.Add(tokens[0]);
+                comboPollutant1.Items.Add(tokens[0]);
+                comboPollutant2.Items.Add(tokens[0]);
+                comboPollutant3.Items.Add(tokens[0]);
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
+            }
+            txtCloseYear.MaxLength = 4;
+            txtOpenYear.MaxLength = 4;
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-
+        if(radioNo.IsChecked == true)
+            {
+                radioNo.IsChecked = false;
+            }
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void radioNo_Checked(object sender, RoutedEventArgs e)
         {
+            if(radioYes.IsChecked == true)
+            {
+                radioYes.IsChecked = false;
+            }
+        }
+
+        public void fillDataGrid()
+        {
+            int startingYear = 0;
+            int closingYear = 0;
+            startingYear = int.Parse(txtOpenYear.Text);
+            closingYear = int.Parse(txtCloseYear.Text);
+            int differnce = closingYear - startingYear;
+            for (int i = 0; i < differnce; i++)
+            {
+
+            }
+        }
+
+        private void txtCloseYear_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int parsedValue;
+            if(!int.TryParse(txtCloseYear.Text, out parsedValue)) //Get rid of any non numerical data
+            {
+                txtCloseYear.Clear();
+            }
+            int countChar = txtOpenYear.Text.ToString().Length;
+            int countCharClose = txtCloseYear.Text.ToString().Length;
+            if (countChar == 4 && countCharClose == 4) //If both values are full of numerical data then create years on data grid.
+            {
+                fillDataGrid();
+            }
+        }
+
+        private void txtOpenYear_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int parsedValue;
+            if (!int.TryParse(txtOpenYear.Text, out parsedValue)) //Get rid of any non numerical data
+            {
+                txtOpenYear.Clear();
+            }
+            int countChar = txtOpenYear.Text.ToString().Length;
+            int countCharClose = txtCloseYear.Text.ToString().Length;
+            if(countChar == 4 && countCharClose == 4) //If both values are full of numerical data then create years on data grid.
+            {
+                fillDataGrid();
+            }
 
         }
     }
 }
+
---
+++ OpenCSV.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public class GetCSV
+{
+    public List<PollutantsObj> pollutants { get; set; } = getData();
+    public static List<PollutantsObj> getData()
+    {
+        var file = @"C:\Users\David\Documents\Projects\lwrncProject\lwrncLandgemWPF\content\pollutants.csv";
+        var lines = File.ReadAllLines(file);
+        var result = new List<PollutantsObj>();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i].Split(',');
+            var GetPollutants = new PollutantsObj()
+            {
+                Compound = line[0],
+                Concentration = line[1],
+                MolecularWeight = line[2]
+            };
+            result.Add(GetPollutants);
+        }
+        return result;
+    }
+}
+
+public class PollutantsObj
+{
+    public string Compound { get; set; }
+    public string Concentration { get; set; }
+    public string MolecularWeight { get; set; }
+}
